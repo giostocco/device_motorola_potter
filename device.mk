4ac5f09113ef01cd8d47873cfb1f3e1b46d2c7c5
eduardobastier93 <eduardobastier93@gmail.com>
2019-05-27 13:17:38 +0000

    potter: Use Moto Camera instead of Camera2

--- device.mk
+++ device.mk
@@ -106,7 +106,7 @@ PRODUCT_PACKAGES += \
     camera.device@3.2-impl \
     libbson \
     libjustshoot_shim \
-    Snap \
+    Camera \
     vendor.qti.hardware.camera.device@1.0 \
     vendor.qti.hardware.camera.device@1.0_vendor
 
