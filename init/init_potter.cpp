6ced06653b1ad712024c1717caa88be5d77bfa36
zilbertojunior <priv.zjr@gmail.com>
2019-06-05 13:03:06 -0300

    potter: use sku string to partially define device model

--- init/init_potter.cpp
+++ init/init_potter.cpp
@@ -76,8 +76,10 @@ void vendor_load_properties()
         return;
 
     // sku
-    std::string sku = android::base::GetProperty("ro.boot.hardware.sku", "");
-    property_override_dual("ro.product.model", "ro.vendor.product.model", sku.c_str());
+    std::string sku = "Moto G5 Plus (";
+    sku.append(android::base::GetProperty("ro.boot.hardware.sku", ""));
+    sku.append(")");
+    property_set("ro.product.model", sku.c_str());
 
     // fingerprint
     property_override("ro.build.description", "potter-7.0/NPNS25.137-33-11/11:user/release-keys");
